051db1b327d4d07f05c55405948c47ecbf3b3adc
PharrellWANG <15113029g@connect.polyu.hk>
2017-03-28 13:22:30 +0800

    working now

--- liveability/models.py
+++ liveability/models.py
@@ -2,4 +2,13 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+
 # Create your models here.
+class Scores(models.Model):
+    district_en = models.CharField(max_length=45, null=True)
+    district_ch = models.CharField(max_length=45, null=True)
+    house_cost = models.IntegerField(null=True)
+    dining_cost = models.IntegerField(null=True)
+    crime_rate = models.IntegerField(null=True)
+    edu_rank = models.IntegerField(null=True)
+    # house_cost = models.IntegerField()
