1e4cce5b8415c0a5bc00dcba42aabcc58af81055
PharrellWANG <15113029g@connect.polyu.hk>
2017-03-31 12:32:08 +0800

    updates

--- project_liveability/settings.py
+++ project_liveability/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'project_liveability.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'liveability',
         'USER': 'postgres',
         'PASSWORD': 'bitnami',
